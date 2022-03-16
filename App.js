0c1f62be2618a019e8c2bdee1276bf1278a38ddb
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-16 22:05:30 +0330

    extend App component

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.taskWrapper}>
+     
+        {/* Today's tasks */}
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+        <View style={styles.items}></View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EAED",
+    border: "3px solid red",
   },
+  taskWrapper: {
+    border: "3px solid blue",
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {},
 });
